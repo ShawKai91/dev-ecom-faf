ef13bf33be554667968731ca11149590e2a9d40d
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-10-28 13:29:57 -0600

    implemented set purchase detail action creator and reducer

--- src/components/account/purchases.js
+++ src/components/account/purchases.js
@@ -16,9 +16,10 @@ class Purchases extends Component {
             <div className={`${className} purchases`}> {
                     this.props.purchases.map(purchase => {
                         return (
-                            <div key={purchase._id} className='purchases__purchase purchase'>
+                            <a className='purchases__purchase purchase' key={purchase._id} 
+                            onClick={() => this.props.setPurchaseDetail(purchase._id)}>
                                 <img className='purchase__img' src='http://via.placeholder.com/80x80'/>
-                            </div>
+                            </a>
                         )
                     })
                 }
