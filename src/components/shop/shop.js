9894cac20f8fa5d873acf28be4c5ea4ac9b707f4
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-11-01 13:10:16 -0700

    structured shop cart component and styled

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,8 +4,17 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
 
 class Shop extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            showCart: true
+        }
+    }
 
     componentDidMount() {
         const headerLinks = [
@@ -17,9 +26,11 @@ class Shop extends Component {
         ]
         this.props.setHeaderLinks(headerLinks);
         this.props.fetchShopCategories();
+
         // filter products with links
         this.props.fetchShopProducts();
     }
+
     shouldComponentUpdate(nextProps) {
         if(this.props != nextProps) {
             this.props.setNavbarLinks(nextProps.categories, (_id) => this.props.filterProductsWithCategoryId(_id));
@@ -32,10 +43,13 @@ class Shop extends Component {
     }
 
     render() {
+        return <ShopCart className='shop__cart'/>
+
         return (
             <div className='shop'>
-                <ShopSearchBar className='shop__search-bar'  onSubmit={this.onSubmit} />
-                <div className='shop__products'> {
+                <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
+                <div className='shop__products'>
+                    {
                         this.props.filteredProducts.map(product => {
                             return (
                                 <ShopProduct {...product} key={product._id} />
@@ -43,6 +57,10 @@ class Shop extends Component {
                         })
                     }
                 </div>
+                {
+                    this.state.showCart ? <ShopCart className='shop__cart'/> : ''
+                }
+                
                 {/* shop cart button */}
             </div>
         )
