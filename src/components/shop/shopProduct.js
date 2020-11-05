713944e60f5000837ceeff4a2078f1ac3d588f5a
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-11-04 19:50:54 -0700

    implemented add to cart feature to add more cart products to a users cart

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions';
+
 import Quantity from '../quantity';
 import GreenPriceTag from '../greenPriceTag';
 
@@ -7,6 +11,8 @@ class ShopProduct extends Component {
     handleAddToCart = () => {
         if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
             document.getElementById('shop-cart').classList.remove('cart-hidden');
+            const { _id, title, description, price, belongsTo } = this.props;
+            this.props.addCartProduct({ _id, title, description, price, belongsTo });
         } else {
             document.getElementById('shop-cart').classList.add('cart-hidden');
         }
@@ -38,4 +44,5 @@ class ShopProduct extends Component {
     }
 }
 
+ShopProduct = connect(null, actions)(ShopProduct);
 export default ShopProduct;
